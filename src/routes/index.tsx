d806905327d6e7fad1730d24eccc9a7e0c7d5c57
Mark Francis <mrkfrancis8@gmail.com>
2024-04-14 18:21:58 +0100

    Added staging TOML APP_NAME

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,7 +8,9 @@ function HomeComponent() {
   return (
     <div className="p-2 flex justify-center">
       <div className="w-full max-w-screen-md">
-        <h3 className="text-2xl">Hello!</h3>
+        <h3 className="text-2xl">
+          Hello from {import.meta.env.VITE_APP_TITLE}!
+        </h3>
       </div>
     </div>
   );
