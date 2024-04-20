794ce8210d87a31386679708046a51510d326a64
Mark Francis <mark@moonstar.ai>
2024-04-20 14:04:39 +0100

    Allow all requests to go through to tRPC

--- functions/api/[[trpc]].ts
+++ functions/api/[[trpc]].ts
@@ -3,7 +3,7 @@ import { appRouter } from "../../api/router";
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const onRequestGet: PagesFunction<Env> = async (req) => {
+export const onRequest: PagesFunction<Env> = async (req) => {
   return fetchRequestHandler({
     endpoint: "/api",
     req: req.request,
